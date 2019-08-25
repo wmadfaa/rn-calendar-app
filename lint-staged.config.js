b8313501f0c2932751be8b28998c67b29527b5c8
wasim almadfaa <wm.madfaa@gmail.com>
2019-08-25 13:08:22 +0200

    add svgo<svg formater>

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   '.*{js,ts,tsx}': ['eslint --fix', 'prettier --write', 'git add'],
   '.*{md,json,js,ts,tsx}': ['prettier --write', 'git add'],
-  '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
+  '*.{png,jpeg,jpg,gif}': ['imagemin-lint-staged', 'git add'],
+  '*.svg': ['svgo', 'git add'],
 };
