805d063b95e11c6b436fff8ebbeeb65177b749b0
wasim almadfaa <wm.madfaa@gmail.com>
2019-08-25 14:23:47 +0200

    fix svgo configs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
   '.*{js,ts,tsx}': ['eslint --fix', 'prettier --write', 'git add'],
   '.*{md,json,js,ts,tsx}': ['prettier --write', 'git add'],
   '*.{png,jpeg,jpg,gif}': ['imagemin-lint-staged', 'git add'],
-  '*.svg': ['svgo', 'git add'],
+  '*.svg': filenames => filenames.map(filename => `svgo --config .svgo.yml --input ${filename}`),
 };
